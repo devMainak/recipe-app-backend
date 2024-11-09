e9db523aaa2cd72ecce9c9ea665907fd7447263e
bitMan <mrterm404@gmail.com>
2024-11-09 14:24:47 +0530

    fix

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 // Accessing mongoDB connection string
-const mongoURI = process.env.MONGODB;
+// const mongoURI = process.env.MONGODB;
 
 // Function for connecting database
 const initializeDatatbase = async () => {
   try {
-    const connection = await mongoose.connect(mongoURI);
+    const connection = await mongoose.connect("mongodb+srv://neoGStudent:neoGDB2024@neog.zwpra3f.mongodb.net/?retryWrites=true&w=majority&appName=neoG");
     if (connection) {
       console.log("Connected Successfully");
     } else {
